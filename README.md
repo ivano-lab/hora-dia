a6707583c5a878bbd21f5b54b11fcee5cbbdc750
Ívano Fontes <ivanofnts@gmail.com>
2022-03-19 19:23:23 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,19 @@
+# verificador-idade
+
+![](screenshot.png)
+
+Abra o brower e descubra em qual período do dia você está.:+1:
+Não tem como dar errado. :sunglasses:
+
+## Como executar o projeto :rocket:
+
+Basta fazer download do projeto e executar em seu browser. :ok_hand:
+
+## Licença :pencil:
+
+Este projeto esta sob a licença MIT.
+
+Feito com muito :heart: por Ívano Fontes :v: [Entre em contato!](https://www.linkedin.com/in/%C3%ADvano-da-silva-cruz-fontes-509b79152/)
+
+
+ 
---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    hora = 8
+    msg.innerHTML = `Agora sao ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/manha.png'
+        msg.innerHTML += '&#128513;	<strong>Bom dia!</strong>'
+        document.body.style.background = "#e4e1d9";
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagens/tarde.png'
+        msg.innerHTML += '&#128513; <strong>Boa tarde!</strong>'
+        document.body.style.background = "#e09875";
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/noite.png'
+        msg.innerHTML += '&#128513; <strong>Boa noite!</strong>'
+        document.body.style.background = "#16283a";
+    }
+}
